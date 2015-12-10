645c429ac773f6364a1239fb037c52e7bd4aad3f
Kevin Austin <kevinaustin@hotmail.com>
2015-12-09 20:29:14 -0800

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  root 'movies#index'
+   get 'carts/show'
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies, only: [:show, :index]
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'movies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
